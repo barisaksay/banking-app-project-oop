ed090d5e4fb9fbcb2dbf41cabeda044a8cd8b7b7
baris <barish.aksay@gmail.com>
2024-05-23 16:58:53 +0200

    added negative test cases

--- tests/account.spec.js
+++ tests/account.spec.js
@@ -1,5 +1,13 @@
 const Account = require("../account")
 
+beforeEach(() => {
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+});
+
+afterEach(() => {
+    consoleSpy.mockRestore();
+});
+
 test("success: creates account with valid data",()=>{
     let userAccount = new Account("testAccount",100);
     expect(userAccount.accountHolderName).toBe("testAccount")
@@ -7,4 +15,22 @@ test("success: creates account with valid data",()=>{
     expect(userAccount.accountNumber).toBeDefined()
 })
 
+test("invalid username",()=>{
+    let userAccount = new Account(123,100);
+    expect(consoleSpy).toBeCalledWith("Please provide a valid username and deposit")
+})
+
+
+test("invalid deposit 0",()=>{
+    let userAccount = new Account("Baris",0);
+    expect(consoleSpy).toBeCalledWith("Please provide a valid username and deposit")
+  
+})
+
+
+test("invalid deposit negative integer",()=>{
+    let userAccount = new Account("Baris",-10);
+    expect(consoleSpy).toBeCalledWith("Please provide a valid username and deposit")
+  
+})
 
