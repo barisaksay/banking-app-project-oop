b8f1ec60388efdd4b30a8bea6b77c40db201eeaa
baris <barish.aksay@gmail.com>
2024-06-04 20:00:08 +0200

    introduced transactionHistory prop, addBalance func updated to include transactionHistory

--- account.js
+++ account.js
@@ -1,48 +1,51 @@
 class Account {
     constructor(accountHolderName, initialDeposit) {
 
-        if (typeof accountHolderName !== "string" || typeof initialDeposit !=="number" || initialDeposit <= 0) {
-            throw new Error ("Please provide a valid username and deposit");
-        } else{
-            this._accountNumber=createAccountNumber();
+        if (typeof accountHolderName !== "string" || typeof initialDeposit !== "number" || initialDeposit <= 0) {
+            throw new Error("Please provide a valid username and deposit");
+        } else {
+            this._accountNumber = createAccountNumber();
             this.accountHolderName = accountHolderName;
-            this._balance=initialDeposit;
+            this._balance = initialDeposit;
+            this.transactionHistory=[];
+            this.transactionHistory.push({"amount":initialDeposit,"date":new Date()})
         }
-     
+
         function createAccountNumber() {
             return Math.floor(Math.random() * 1000000);
         }
     }
 
-    get balance(){
+    get balance() {
         return this._balance;
     }
 
-   get accountNumber(){
-       return this._accountNumber;
-   }
+    get accountNumber() {
+        return this._accountNumber;
+    }
 
-    get accountHolder(){
+    get accountHolder() {
         return this.accountHolderName;
     }
 
-    set accountHolder(newName){
+    set accountHolder(newName) {
         return this.accountHolderName = newName;
- 
+
     }
-    addBalance(amount){
-        if(amount <= 0 || typeof amount !== 'number'){
-         return "Please enter a valid amount";
-} else{
-        this._balance+=amount;
-        return `You have succesfully added ${amount} USD to your account. Your current balance is ${this._balance}`;
+    addBalance(amount) {
+        if (amount <= 0 || typeof amount !== 'number') {
+            return "Please enter a valid amount";
+        } else {
+            this._balance += amount;
+            this.transactionHistory.push({"amount":amount,"date":new Date()})
+            return `You have succesfully added ${amount} USD to your account. Your current balance is ${this._balance} USD`;
         }
-        
-}
+
+    }
 }
 
-function createAccount(accountHolderName,initialDeposit){
-    return new Account(accountHolderName,initialDeposit) 
+function createAccount(accountHolderName, initialDeposit) {
+    return new Account(accountHolderName, initialDeposit)
 }
 
 module.exports = Account;
