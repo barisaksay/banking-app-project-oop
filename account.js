e3411d756ea773283549f1efc7a79a8caa6adee9
baris <barish.aksay@gmail.com>
2024-06-12 19:30:02 +0200

    refactored getDeposits and getWithdrawals methods.Switched from forEach to filter. Removed extra arrays, now just returning values rather than pushing into new array.

--- account.js
+++ account.js
@@ -7,8 +7,8 @@ class Account {
             this._accountNumber = Math.floor(Math.random() * 1000000);;
             this.accountHolderName = accountHolderName;
             this._balance = initialDeposit;
-            this.transactionHistory=[];
-            this.transactionHistory.push({"action":"deposit","amount":initialDeposit,"date":new Date()})
+            this.transactionHistory = [];
+            this.transactionHistory.push({ "action": "deposit", "amount": initialDeposit, "date": new Date() })
             this.accountCreationDate = new Date()
         }
 
@@ -27,13 +27,13 @@ class Account {
     }
 
     set accountHolder(newName) {
-             if (typeof newName !== "string"){
-                    throw new Error("Please provide a valid username");
-             } else{
-                 return this.accountHolderName = newName;
-}
-        
-        
+        if (typeof newName !== "string") {
+            throw new Error("Please provide a valid username");
+        } else {
+            return this.accountHolderName = newName;
+        }
+
+
 
     }
     addFunds(amount) {
@@ -41,65 +41,50 @@ class Account {
             return "Please enter a valid amount";
         } else {
             this._balance += amount;
-            this.transactionHistory.push({"action":"deposit","amount":amount,"date":new Date()})
+            this.transactionHistory.push({ "action": "deposit", "amount": amount, "date": new Date() })
             return `You have succesfully added ${amount} USD to your account. Your current balance is ${this._balance} USD`;
         }
 
     }
 
-    withdrawFunds(amount){
-        if( amount <= 0 || typeof amount !== 'number'){
+    withdrawFunds(amount) {
+        if (amount <= 0 || typeof amount !== 'number') {
             throw new Error("enter a valid amount");
-        } else if(amount > this._balance){
+        } else if (amount > this._balance) {
             throw new Error(`You do not have enough funds. Your available balance is ${this._balance}USD`);
         }
         else {
             this._balance -= amount;
-            this.transactionHistory.push({"action":"withdrawal" ,"amount":amount,"date":new Date()})
+            this.transactionHistory.push({ "action": "withdrawal", "amount": amount, "date": new Date() })
             return `Withdrawal successful. Your available balance is ${this._balance}`
         }
     }
 
-accountSummary() {
-    // Create a summary string with account details
-    let summary = `Your account summary for account ${this._accountNumber}:`;
-    summary += `Account balance: ${this.balance}`;
-    summary += `Transaction history:`;
+    accountSummary() {
+        // Create a summary string with account details
+        let summary = `Your account summary for account ${this._accountNumber}:`;
+        summary += `Account balance: ${this.balance}`;
+        summary += `Transaction history:`;
 
-    // Loop through the transaction history and add each transaction to the summary
-    for (let i = 0; i < this.transactionHistory.length; i++) {
-         let transactionAction = this.transactionHistory[i].action;
-         let transactionAmount= this.transactionHistory[i].amount;
-         let transactionHistory=this.transactionHistory[i].date;
+        // Loop through the transaction history and add each transaction to the summary
+        for (let i = 0; i < this.transactionHistory.length; i++) {
+            let transactionAction = this.transactionHistory[i].action;
+            let transactionAmount = this.transactionHistory[i].amount;
+            let transactionHistory = this.transactionHistory[i].date;
 
-        summary += `- ${transactionAction} of $${transactionAmount} on ${transactionHistory}`;
+            summary += `- ${transactionAction} of $${transactionAmount} on ${transactionHistory}`;
+        }
+
+        return summary;
     }
 
-    return summary;
-}
+    getDeposits() {
+        return this.transactionHistory.filter((item) => item.action == "deposit")
+    }
 
- getDeposits(){
-    let deposits=[]
-    this.transactionHistory.forEach((item)=>{
-        if(item.action=="deposit"){
-            deposits.push(item);
-        }
-    })
-
-    return deposits;
- }
-
- getWithdrawals(){
-    let withdrawals=[]
-    this.transactionHistory.forEach((item)=>{
-        if(item.action=="withdrawal"){
-            withdrawals.push(item);
-        }
-    })
-
-    return withdrawals;
- 
-} 
+    getWithdrawals() {
+        return this.transactionHistory.filter((item) => item.action == "withdrawal")
+    }
 }
 
 
